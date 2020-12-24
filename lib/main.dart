e6455fe51713c05f38a88a44ac4b3317ec084818
Nicolás Siplis <25803723+nicolas-siplis@users.noreply.github.com>
2020-12-24 00:04:45 -0300

    UI improvements.

--- lib/main.dart
+++ lib/main.dart
@@ -22,13 +22,24 @@ class _MyAppState extends State<MyApp> {
   String _error = "";
   final textRecognizer = FirebaseVision.instance.textRecognizer();
   int bufferSize;
-  List<List<String>> matrix = [];
-  List<List<String>> sequences = [];
+  List<List<String>> matrix = [
+    ["1C", "BD", "55", "E9", "55"],
+    ["1C", "BD", "1C", "55", "E9"],
+    ["55", "E9", "E9", "BD", "BD"],
+    ["55", "FF", "FF", "1C", "1C"],
+    ["FF", "E9", "1C", "BD", "FF"]
+  ];
+  List<List<String>> sequences = [
+    ["1C", "1C", "55"],
+    ["55", "FF", "1C"],
+    ["BD", "E9", "BD", "55"],
+    ["55", "1C", "FF", "BD"]
+  ];
   final List<String> _validHex = ["1C", "FF", "E9", "BD", "55"];
   String _processing;
-  String _solution;
+  Path _solution = Path([]);
 
-  static String _calculateSolution(map) {
+  static Path _calculateSolution(map) {
     List<List<String>> matrix = map["matrix"];
     List<List<String>> sequences = map["sequences"];
     int bufferSize = map["bufferSize"];
@@ -42,7 +53,7 @@ class _MyAppState extends State<MyApp> {
         maxPath = path;
       }
     }
-    return maxPath.coords.map((coord) => "\n(${coord[0]}, ${coord[1]}) -> ${matrix[coord[0]][coord[1]]}").join("");
+    return maxPath;
   }
 
   void computeSolution(String processing) {
@@ -52,6 +63,7 @@ class _MyAppState extends State<MyApp> {
       setState(() {});
       compute(_calculateSolution, {"bufferSize": bufferSize, "matrix": matrix, "sequences": sequences}).then((solution) {
         _solution = solution;
+        _processing = null;
         setState(() {});
       });
     }
@@ -62,25 +74,30 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
+            centerTitle: true,
+            backgroundColor: Colors.red,
             title: const Text('Cyberpwned'),
           ),
           body: Container(
-            padding: EdgeInsets.all(16),
+            color: Colors.black,
             child: ListView(
               children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    ElevatedButton(
-                      child: Text('Upload Breach Screen'),
+                SizedBox(height: 40),
+                SizedBox(
+                    height: 40,
+                    child: ElevatedButton(
+                      child: Text('Upload New Breach Screen'),
                       onPressed: () async {
                         try {
                           _error = "";
-                          _solution = null;
+                          _solution = Path([]);
                           matrix = [];
                           sequences = [];
 
                           var file = await ImagePicker.pickImage(source: ImageSource.camera);
+                          if (file == null) {
+                            return;
+                          }
                           final FirebaseVisionImage visionImage = FirebaseVisionImage.fromFile(file);
                           final VisionText visionText = await textRecognizer.processImage(visionImage);
 
@@ -94,62 +111,59 @@ class _MyAppState extends State<MyApp> {
                             throw Exception("Invalid size");
                           }
                         } catch(e) {
-                          _error = "There was an error processing the breach screen. Please try again, and remember that a better quality photo improves the chances of parsing it correctly.";
+                          _error = "There was an error processing the breach screen. Make sure that both the code matrix and the sequences are clearly visible, and remember that a better quality photo improves the chances of parsing it correctly.";
                         }
                         computeSolution("Calculating optimal path...");
                       },
-                    ),
-                  ],
-                ),
+                )),
+                SizedBox(height: 40),
                 SizedBox(
-                  height: 16,
-                ),
-                SizedBox(
-                    height: 16,
+                    height: 65,
                     child: TextField(
-                        decoration: new InputDecoration(labelText: "Input your buffer size"),
+                        style: TextStyle(color: Colors.white),
+                        decoration: new InputDecoration(fillColor: Colors.white, focusColor: Colors.white, hoverColor: Colors.white, labelText: "Input your buffer size", labelStyle: TextStyle(color: Colors.white)),
                         keyboardType: TextInputType.number,
+                        cursorColor: Colors.white,
                         inputFormatters: <TextInputFormatter>[FilteringTextInputFormatter.digitsOnly],
                         onSubmitted: (buffer) async {
                           int newBuffer = int.parse(buffer, radix: 10);
                           if (newBuffer != bufferSize) {
+                            String processing = bufferSize == null ? "Calculating optimal path..." : "Recalculating optimal path because of buffer size change...";
                             bufferSize = newBuffer;
-                            computeSolution("Recalculating optimal path because of buffer size change...");
+                            computeSolution(processing);
+                            setState(() {});
                           }
                         })
                 ),
-                SizedBox(
-                  height: 16,
-                ),
+                Column(children: [
+                      Text(_processing != null ? _processing : "", style: TextStyle(color: Colors.white)),
+                      Container(
+                        child: Table(children: matrix.asMap().entries
+                            .map((row) => TableRow(children: row.value.asMap().entries
+                            .map((column) => Padding(padding: EdgeInsets.all(2), child: Container(color: _isPartOfSolution(row.key, column.key) == null ? Colors.lightBlue : Colors.green, child: Text(_isPartOfSolution(row.key, column.key)?.toString() ?? column.value, textAlign: TextAlign.center, style: TextStyle(fontSize: 20))))).toList())).toList())),
+                      SizedBox(height: 16),
+                      Container(
+                          child: Table(children: sequences.map((row) => row + List.filled(max(0, sequences.map((r) => r.length).fold(0, max) - row.length), "")).map((row) => TableRow(children: row.map((elm) => Padding(padding: EdgeInsets.symmetric(vertical: 2), child: Container(color: elm.isEmpty ? Colors.transparent : (SequenceScore(row.where((e) => e != "").toList(), bufferSize).isCompletedBy(_solution, matrix) ? Colors.green : Colors.amber), child: Text(elm, textAlign: TextAlign.center, style: TextStyle(fontSize: 20))))).toList())).toList())
+                      )
+                ]),
                 Text(
                   _error,
-                  style: TextStyle(color: Colors.red),
+                  style: TextStyle(backgroundColor: Colors.red, color: Colors.white),
+                  textAlign: TextAlign.justify
                 ),
-                SizedBox(
-                  height: 16,
-                ),
-                Row(
-                children: [
-                  Expanded(
-                      flex: 5,
-                      child: Column(children: [
-                        Text(_processing != null ? _processing : ""),
-                        Text(matrix.length != 0 ? "Parsed Matrix:" : ""),
-                        Text(matrix.length != 0 ? matrix.map((row) => "\n" + row.join(" - ") + "\n").toString().replaceAll(",", "").replaceAll("(", "").replaceAll(")", "") : ""),
-                        Text(sequences.length != 0 ? "Parsed Sequences:" : ""),
-                        Text(sequences.length != 0 ? sequences.map((row) => "\n" + row.join(" - ") + "\n").toString().replaceAll(",", "").replaceAll("(", "").replaceAll(")", "") : "")
-                  ])),
-                      Expanded(
-                        flex: 5,
-                        child: Column(children: [
-                          Text(_solution != null ? "Maximum Reward Path:" : ""),
-                          Text(_solution != null ? _solution : "")
-                      ]))
-                ]),
               ],
             ),
           )),
     );
+  }
+
+  int _isPartOfSolution(int row, int column) {
+    for (int i = 0; i < _solution.coords.length; i++) {
+      if (_solution.coords[i][0] == row && _solution.coords[i][1] == column) {
+        return i+1;
+      }
+    }
+    return null;
   }
 }
 
@@ -259,6 +273,19 @@ class SequenceScore {
     }
   }
 
+  bool isCompletedBy(Path path, List<List<String>> matrix) {
+    path.coords.forEach((coord) => compute(matrix[coord[0]][coord[1]]));
+    return score == _maxScore();
+  }
+
+  int _maxScore() {
+    return pow(10, rewardLevel + 1);
+  }
+
+  int _minScore() {
+    return -rewardLevel - 1;
+  }
+
   // When the head of the sequence matches the targeted node, increase the score by 1
   // If the sequence has been completed, set the score depending on the reward level
   void _increase() {
@@ -267,7 +294,7 @@ class SequenceScore {
     if (_completed()) {
       // Can be adjusted to maximize either:
       //  a) highest quality rewards, possibly lesser quantity
-      score = pow(10, rewardLevel + 1);
+      score = _maxScore();
       //  b) highest amount of rewards, possibly lesser quality
       // this.score = 100 * (this.rewardLevel + 1);
     }
@@ -277,11 +304,11 @@ class SequenceScore {
   // If it's not possible to complete the sequence, set the score to a negative value depending on the reward
   void _decrease() {
     this.bufferSize--;
-    if (this.score > 0) {
+    if (score > 0) {
       score--;
     }
-    if (this._completed()) {
-      score = -rewardLevel - 1;
+    if (_completed()) {
+      score = _minScore();
     }
   }
 
