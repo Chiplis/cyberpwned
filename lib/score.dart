0b4816479514e7825648f4a521298d9b127afe20
Nicolás Siplis <25803723+nicolas-siplis@users.noreply.github.com>
2021-01-11 01:22:42 -0300

    Revert pathfinding rules change.

--- lib/score.dart
+++ lib/score.dart
@@ -29,14 +29,6 @@ class SequenceScore {
       return score;
     }
 
-    if (progress > 0 && sequence[progress] != compare && sequence[progress - 1] == compare) {
-      bufferSize--;
-      if (bufferSize < maxProgress - progress) {
-        score = minScore();
-      }
-      return score;
-    }
-
     int oldProgress = progress;
     progress += sequence[progress] == compare ? _increase() : _decrease();
     score += (progress - oldProgress) * pow(10, rewardLevel);
@@ -86,7 +78,7 @@ class PathScore {
   int score;
   TraversedPath path;
   int bufferSize;
-  List<SequenceScore> sequenceScores = List<SequenceScore>();
+  List<SequenceScore> sequenceScores = [];
   CellGroup matrix;
   static Map<TraversedPath, PathScore> previousScores = {};
 
