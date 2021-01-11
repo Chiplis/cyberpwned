d9e2249836cf3f534bb8be7eb1d59eab176746a2
Nicolás Siplis <25803723+nicolas-siplis@users.noreply.github.com>
2021-01-11 01:50:23 -0300

    Revert "Revert pathfinding rules change."
    This reverts commit 0b4816479514e7825648f4a521298d9b127afe20.

--- lib/score.dart
+++ lib/score.dart
@@ -29,6 +29,14 @@ class SequenceScore {
       return score;
     }
 
+    if (progress > 0 && sequence[progress] != compare && sequence[progress - 1] == compare) {
+      bufferSize--;
+      if (bufferSize < maxProgress - progress) {
+        score = minScore();
+      }
+      return score;
+    }
+
     int oldProgress = progress;
     progress += sequence[progress] == compare ? _increase() : _decrease();
     score += (progress - oldProgress) * pow(10, rewardLevel);
@@ -78,7 +86,7 @@ class PathScore {
   int score;
   TraversedPath path;
   int bufferSize;
-  List<SequenceScore> sequenceScores = [];
+  List<SequenceScore> sequenceScores = List<SequenceScore>();
   CellGroup matrix;
   static Map<TraversedPath, PathScore> previousScores = {};
 
